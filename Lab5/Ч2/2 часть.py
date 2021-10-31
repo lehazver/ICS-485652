54b97beed49b66b91c8e685e473692ca9d606215
Oleksiy Zvirkovskiy <90178248+lehazver@users.noreply.github.com>
2021-11-01 01:32:59 +0200

    Add files via upload

---
+++ Lab5/Ч2/2 часть.py
@@ -0,0 +1,2 @@
+import random
+print (random.choice(open("Wordlist1.txt").readline().split()),random.choice(open("Wordlist2.txt").readline().split()),random.choice(open("Wordlist3.txt").readline().split()))
