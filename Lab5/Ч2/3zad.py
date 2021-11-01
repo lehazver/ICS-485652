d69eb92edf8373c27c52950c9a4ea4a023162ef2
Oleksiy Zvirkovskiy <90178248+lehazver@users.noreply.github.com>
2021-11-01 02:12:02 +0200

    Add files via upload

---
+++ Lab5/Ч2/3zad.py
@@ -0,0 +1,21 @@
+file=open("book.txt")
+file = open("book.txt", encoding="utf8")
+sentences = 0
+loudsentences = 0
+questions = 0
+longsentences = 0
+for i in file:
+    i = i[0:-1]
+    for j in i:
+        if j=='.' or j== '!' or j== '!':
+            sentences+= 1
+    for k in i:
+        if k== '!':
+            loudsentences+=1
+    for l in i:
+        if l=='?':
+            questions+=1
+    for a in i:
+        if a=='...':
+            longsentences+=1
+print("sentences:", sentences, "loudsentences:", loudsentences, "questions:", questions, "longsentences:", longsentences)
